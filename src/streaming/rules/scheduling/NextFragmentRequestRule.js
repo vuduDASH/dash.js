a0d66d347145e9ccf316298586c4dc20f35ae390
Rik Sagar [Vudu] <rik.sagar@vudu.com>
2017-07-07 15:14:48 -0700

    Workaround for buffer truncation issues: Check that source buffer contains complete and contiguous fragments - detects silent dropping of data by MediaSource. Fix seek issue:  Problem in logic if there's a quality switch and a seek at the same time. Workaround fragment tolerence issue: Fragments are contiguous if the gap between them is less than size of a video frame.  Default was too large.

--- src/streaming/rules/scheduling/NextFragmentRequestRule.js
+++ src/streaming/rules/scheduling/NextFragmentRequestRule.js
@@ -29,6 +29,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  */
 import Debug from '../../../core/Debug';
+import PlaybackController from '../../controllers/PlaybackController';
 import FactoryMaker from '../../../core/FactoryMaker';
 
 function NextFragmentRequestRule(config) {
@@ -39,6 +40,12 @@ function NextFragmentRequestRule(config) {
     const sourceBufferController = config.sourceBufferController;
     const textSourceBuffer = config.textSourceBuffer;
 
+    let playbackController;
+
+    function setup() {
+        playbackController = PlaybackController(context).getInstance();
+    }
+
     function execute(streamProcessor, requestToReplace) {
 
         const representationInfo = streamProcessor.getCurrentRepresentationInfo();
@@ -48,35 +55,88 @@ function NextFragmentRequestRule(config) {
         const seekTarget = scheduleController.getSeekTarget();
         const hasSeekTarget = !isNaN(seekTarget);
         const buffer = streamProcessor.getBuffer();
+        const playbackTime = playbackController.getTime();
 
         let time = hasSeekTarget ? seekTarget : adapter.getIndexHandlerTime(streamProcessor);
+        let hasTimeAdjusted = false;
 
         if (isNaN(time) || (mediaType === 'fragmentedText' && textSourceBuffer.getAllTracksAreDisabled())) {
             return null;
         }
 
         if (hasSeekTarget) {
-            scheduleController.setSeekTarget(NaN);
+            if (!!requestToReplace) {
+                // VUDU Rik - This situtation occurs if there's a quality switch around the same time that there's seek.
+                log('[', mediaType, '] WARN: Performing replace in NextFragmentRule, but seek has been requested.  Preserving seek target until next time');
+            }
+            else {
+                scheduleController.setSeekTarget(NaN);
+            }
         }
 
         /**
          * This is critical for IE/Safari/EDGE
          * */
         if (buffer) {
-            const range = sourceBufferController.getBufferRange(streamProcessor.getBuffer(), time);
+            const range = sourceBufferController.getBufferRange(buffer, time);
             if (range !== null) {
-                log('Prior to making a request for time, NextFragmentRequestRule is aligning index handler\'s currentTime with bufferedRange.end.', time, ' was changed to ', range.end);
+                log('[' + mediaType + '] Prior to making a request for time, NextFragmentRequestRule is aligning index handler\'s currentTime with bufferedRange.end.', time, ' was changed to ', range.end);
+                if (Math.abs(range.end - time) > 0.5) {
+                    hasTimeAdjusted = true;
+                }
                 time = range.end;
             }
         }
+
+        // VUDU Rik - Check here to see if there's any gap between the current playback position buffer, and the head of the buffer where we're appending
+        // if the buffer is not contiguous between the two, then either we have trimmed, or the platform has silently trimmed.
+        if (buffer && !hasSeekTarget && !isNaN(playbackTime) && (time > playbackTime)) {
+            log('[', mediaType, '] time > playbackTime');
+
+            const range = sourceBufferController.getBufferRange(buffer, playbackTime, null /*tolerance*/);
+            if (null === range) {
+                log('[', mediaType, '] playbackTime range === null');
+                // Need to establish buffer at playback time!
+                time = playbackTime;
+                // RIK - is this 'hasTimeAdjusted' idea really correct?
+                hasTimeAdjusted = true;
+            }
+            else {
+                const playheadBufferEnd = range.end;
+                var reqForPlayhead = adapter.getFragmentRequestForTime(streamProcessor, representationInfo, playheadBufferEnd, {timeThreshold: 0, keepIdx: false});
+                if ( reqForPlayhead && ((reqForPlayhead.startTime + reqForPlayhead.duration - playheadBufferEnd) > 2 / 30) ) {
+                    log('[', mediaType, '] Partial fragment - reqNow: ', reqForPlayhead.startTime, ' - ', reqForPlayhead.startTime + reqForPlayhead.duration, '; playheadBufferEnd: ', playheadBufferEnd, '; Repeating playhead fragment');
+                    // Partial fragment, re-request fragment
+                    time = playheadBufferEnd;
+                    hasTimeAdjusted = true;
+
+                    if (!!requestToReplace) {
+                        log('[', mediaType, '] Already here for a replace, ignoring time adjust for now');
+                    }
+                    else {
+                        requestToReplace = reqForPlayhead;
+                        adapter.setIndexHandlerTime(streamProcessor, requestToReplace.startTime + requestToReplace.duration);
+                    }
+
+                }
+            }
+        }
+
 
         let request;
         if (requestToReplace) {
             time = requestToReplace.startTime + (requestToReplace.duration / 2);
             request = adapter.getFragmentRequestForTime(streamProcessor, representationInfo, time, {timeThreshold: 0, ignoreIsFinished: true});
         } else {
-            request = adapter.getFragmentRequestForTime(streamProcessor, representationInfo, time, {keepIdx: !hasSeekTarget});
+            request = adapter.getFragmentRequestForTime(
+                streamProcessor, representationInfo, time,
+                {
+                    timeThreshold: 2 / 30,
+                    keepIdx: !hasSeekTarget && !hasTimeAdjusted
+                });
+
             if (request && streamProcessor.getFragmentModel().isFragmentLoaded(request)) {
+                log('[', mediaType, '] NextFragmentRequestRule request.index = ', request.index, ' for time = ', time, ' is already loaded!!');
                 request = adapter.getNextFragmentRequest(streamProcessor, representationInfo);
             }
             if (request) {
@@ -85,7 +145,9 @@ function NextFragmentRequestRule(config) {
                 scheduleController.setTimeToLoadDelay(0);
             }
         }
-
+        if (request) {
+            log('[', mediaType, '] NextFragmentRequestRule got request.index = ', request.index, ' request.startTime = ', request.startTime, ' for ', (hasSeekTarget ? 'seekTarget = ' : 'time = '), time);
+        }
         return request;
     }
 
@@ -93,6 +155,7 @@ function NextFragmentRequestRule(config) {
         execute: execute
     };
 
+    setup();
     return instance;
 }
 
